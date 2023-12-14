b44db19a5fb9058a97410d52952c60419d6ac98c
Félix Jiménez Calvo <felixjcalvo@gmail.com>
2023-12-14 19:52:50 +0100

    Add files via upload

---
+++ one/Program.cs
@@ -0,0 +1,18 @@
+﻿static int One()
+{
+    List<int> nums = [2, 1, 3, 5, 3, 2];
+    HashSet<int> alreadyPresent = [];
+
+    int result = -1;
+    foreach (int num in nums)
+    {
+        if (!alreadyPresent.Add(num))
+        {
+            result = num;
+            break;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(One());
