c6c20fdde5df0542c5a2250460e962e701c3a93e
FELIX JIMENEZ <felix.jimenez.sp@lufthansa-group.com>
2023-12-15 10:42:25 +0100

    Signed-off-by: FELIX JIMENEZ <felix.jimenez.sp@lufthansa-group.com>

---
+++ challenges/Two.cs
@@ -0,0 +1,24 @@
+﻿static List<string> Two(List<string> words, string letters)
+{
+    List<string> presents = [];
+    HashSet<char> hashLetters = new(letters);
+    foreach (var word in words)
+    {
+        HashSet<char> hashWord = new(word);
+
+        if (hashWord.IsSubsetOf(hashLetters))
+        {
+            presents.Add(word);
+        }
+    }
+    return presents;
+}
+
+List<string> words = ["tren", "oso", "pelota"];
+string letters = "tronesa";
+
+List<string> result = Two(words, letters);
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
